bdaaa0aa562c84ae17b4f9211436098906d6baa4
Muhammad Kashif <34652365+mkashif113@users.noreply.github.com>
2020-03-23 02:09:14 +0500

    replace logo

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -29,7 +29,7 @@ const Topbar = props => {
         <RouterLink to="/">
           <img
             alt="Logo"
-            src="/images/logos/logo--white.svg"
+            src="/images/logos/twitterati.svg"
           />
         </RouterLink>
         <div className={classes.flexGrow} />
